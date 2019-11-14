24a037feca945c081020618645093904888098ed
jaybenaim <benaimjacob@gmail.com>
2019-11-13 19:47:19 -0500

    mobile styling for projects page

--- js/main.js
+++ js/main.js
@@ -3,10 +3,7 @@ $(function() {
     e.preventDefault();
     sendEmailToGmail();
   });
-  $(".email-2-btn").click(e => {
-    e.preventDefault();
-    sendEmailToICloud();
-  });
+
   $(".phone-number").click(() => {
     window.open("tel:6476404714");
   });
@@ -16,10 +13,4 @@ const sendEmailToGmail = () => {
   const body = "Hey Jacob, ";
 
   window.open(`mailto:benaimjacob@gmail.com?subject=${subject}&body=${body}`);
-};
-const sendEmailToICloud = () => {
-  const subject = "Portfolio Contact Form";
-  const body = "Hey Jacob, ";
-
-  window.open(`mailto:jacob.benaim@icloud.com?subject=${subject}&body=${body}`);
 };
