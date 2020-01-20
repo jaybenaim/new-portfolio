9e7545fbf006d3dedf03b0b1b422d540cf597f22
jaybenaim <benaimjacob@gmail.com>
2020-01-20 15:57:01 -0500

    added hover challenges for shop-it

---
+++ js/projects.js
@@ -0,0 +1,30 @@
+$(function() {
+  const textContainer = $(".hidden-challenge-container");
+  const hiddenText = $(".hidden-challenge-text");
+
+  textContainer.hover(
+    function() {
+      $(this)
+        .find(hiddenText)
+        .show();
+    },
+    () => {
+      $(this)
+        .find(hiddenText)
+        .hide();
+    }
+  );
+});
+$(".phone-number").hover(
+  function() {
+    $(".phone-number").hide();
+    $(".phone-number-hidden").show();
+    $(".phone-number-hidden").css("font-size", "1.6em");
+  },
+  function() {
+    $(this)
+      .find("span")
+      .last()
+      .remove();
+  }
+);
