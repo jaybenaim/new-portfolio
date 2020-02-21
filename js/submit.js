e02244c916b82a106178718bc7034f4dcd4ee968
jaybenaim <benaimjacob@gmail.com>
2020-02-20 22:21:00 -0500

    added form

---
+++ js/submit.js
@@ -0,0 +1,24 @@
+const form = document.querySelector("form");
+const displayAlert = data => {
+  const { name, email, message } = data.body;
+  const messageConfirmed = `<div> <br /> Message sent to ${email}.<br />  </div> <br /> <div>Message: <p> ${message} <br />  from ${name}.</p> </div> `;
+  $("form").append(messageConfirmed);
+};
+
+$(function() {
+  form.addEventListener("submit", function(e) {
+    e.preventDefault();
+    const formattedFormData = new FormData(form);
+    postData(formattedFormData);
+  });
+});
+async function postData(formattedFormData) {
+  const response = await fetch("http://localhost/home/newname.php", {
+    method: "POST",
+    body: formattedFormData
+  });
+  const data = await response.text();
+
+  const json = JSON.parse(data);
+  displayAlert(json);
+}
