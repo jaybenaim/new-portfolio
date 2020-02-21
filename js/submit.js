7d8008ffe39461a7c95e347f7048d27ce144c9a7
jaybenaim <benaimjacob@gmail.com>
2020-02-20 23:21:04 -0500

    hid php form

--- js/submit.js
+++ js/submit.js
@@ -1,24 +1,24 @@
-$(function() {
-  const form = document.querySelector("form");
-  const displayAlert = data => {
-    const { name, email, message } = data.body;
-    const messageConfirmed = `<div> <br /> Message sent to ${email}.<br />  </div> <br /> <div>Message: <p> ${message} <br />  from ${name}.</p> </div> `;
-    $("form").append(messageConfirmed);
-  };
+// $(function() {
+//   const form = document.querySelector("form");
+//   const displayAlert = data => {
+//     const { name, email, message } = data.body;
+//     const messageConfirmed = `<div> <br /> Message sent to ${email}.<br />  </div> <br /> <div>Message: <p> ${message} <br />  from ${name}.</p> </div> `;
+//     $("form").append(messageConfirmed);
+//   };
 
-  form.addEventListener("submit", function(e) {
-    e.preventDefault();
-    const formattedFormData = new FormData(form);
-    postData(formattedFormData);
-  });
-  async function postData(formattedFormData) {
-    const response = await fetch("../php/submit.php", {
-      method: "POST",
-      body: formattedFormData
-    });
-    const data = await response.text();
+//   form.addEventListener("submit", function(e) {
+//     e.preventDefault();
+//     const formattedFormData = new FormData(form);
+//     postData(formattedFormData);
+//   });
+//   async function postData(formattedFormData) {
+//     const response = await fetch("https://jacobbenaim.ca/php/submit.php", {
+//       method: "POST",
+//       body: formattedFormData
+//     });
+//     const data = await response.text();
 
-    const json = JSON.parse(data);
-    displayAlert(json);
-  }
-});
+//     const json = JSON.parse(data);
+//     displayAlert(json);
+//   }
+// });
