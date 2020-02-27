d1578a9a69f9023cee437335ecb2d62879748f61
jaybenaim <benaimjacob@gmail.com>
2020-02-26 19:17:03 -0500

    cta with experss and sendgrid api

--- backend/index.js
+++ backend/index.js
@@ -3,12 +3,22 @@ const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
 const db = require("./queries");
+const sgMail = require("@sendgrid/mail");
+const cors = require("cors");
+
 app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
     extended: true
   })
 );
+
+app.use(cors());
+
+require("dotenv").config();
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
 app.get("/", (request, response) => {
   response.json({ info: "Node.js, Express, and Postgres API" });
 });
@@ -19,6 +29,28 @@ app.put("/users/:id", db.updateUser);
 app.delete("/users/:id", db.deleteUser);
 
 process.on("warning", e => console.warn(e.stack));
+
+app.post("/email", (req, res, next) => {
+  const { name, email, message } = req.body;
+  let html = `<div>${message} <br /> from ${name}</div>`;
+  let subject = `Portfolio Contact - ${name}`;
+  const msg = {
+    to: "benaimjacob@gmail.com",
+    from: email,
+    subject,
+    text: message,
+    html
+  };
+  sgMail
+    .send(msg)
+    .then(res => {
+      console.log("Success");
+    })
+    .catch(err => {
+      console.log("Error", err);
+    });
+  res.send({ name, email, message });
+});
 
 app.listen(port, () => {
   console.log(`App running on port ${port}.`);
--- js/submit.js
+++ js/submit.js
@@ -1,24 +1,51 @@
-// $(function() {
-//   const form = document.querySelector("form");
-//   const displayAlert = data => {
-//     const { name, email, message } = data.body;
-//     const messageConfirmed = `<div> <br /> Message sent to ${email}.<br />  </div> <br /> <div>Message: <p> ${message} <br />  from ${name}.</p> </div> `;
-//     $("form").append(messageConfirmed);
-//   };
+const form = document.querySelector("form");
+const displayAlert = data => {
+  const { name, email, message } = data;
 
-//   form.addEventListener("submit", function(e) {
-//     e.preventDefault();
-//     const formattedFormData = new FormData(form);
-//     postData(formattedFormData);
-//   });
-//   async function postData(formattedFormData) {
-//     const response = await fetch("https://jacobbenaim.ca/php/submit.php", {
-//       method: "POST",
-//       body: formattedFormData
-//     });
-//     const data = await response.text();
+  const messageConfirmed = `<div> <br /> Message sent to ${email}.<br />  </div> <br /> <div>Message: <p> ${message} </p>  from ${name}</div> `;
+  $("form").append(messageConfirmed);
+};
 
-//     const json = JSON.parse(data);
-//     displayAlert(json);
-//   }
-// });
+$(function() {
+  form.addEventListener("submit", function(e) {
+    e.preventDefault();
+    const formData = $(".email-form");
+    const formattedFormData = validateForm(formData);
+
+    postData(formattedFormData);
+  });
+});
+async function postData(formattedFormData) {
+  const response = await fetch("http://localhost:3000/email", {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(formattedFormData)
+  });
+  const data = await response.json();
+  displayAlert(data);
+}
+
+const validateForm = form => {
+  const name = form.find("#name").val();
+  const email = form.find("#email").val();
+  const message = form.find("#message").val();
+
+  const regex = /<\w*/gm;
+  const invalidName = name.match(regex);
+  const invalidEmail = email.match(regex);
+  const invalidMessage = message.match(regex);
+
+  if (
+    invalidName === null &&
+    invalidEmail === null &&
+    invalidMessage === null
+  ) {
+    return { name, email, message };
+  } else {
+    alert("Nice try!\n Hack on! ");
+    window.location.href = "http://www.staggeringbeauty.com/";
+  }
+};
